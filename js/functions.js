8f915f643f9190a8f567ff77f27b8d64bc707966
IONIN66 <127221123+IONIN66@users.noreply.github.com>
2024-02-04 23:39:46 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,14 @@
+// 1 Задача:
+
+function palindrome(word) {
+  word = word.replaceAll(' ', '').toLocaleLowerCase();//когда в строке встречаются пробелы.
+  let j = word.length - 1;
+  for (let i = 0; i <= word.length - 1; i++) {
+    if (word[i] !== word[j]){
+      return false;
+    }
+    j--;
+  }
+  return true;
+}
 
