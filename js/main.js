78e1b625bbeee8aafaa371d31cb526cc732571b5
IONIN66 <127221123+IONIN66@users.noreply.github.com>
2024-02-10 13:50:15 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ class CreateObjTwo {
   static creatAuthor() {
     let result = CreateObjTwo.creatNoRepNum(10, 1, CreateObjTwo.allAuthor);
     if (result < 10) {
-      result = '0' + result;
+      result = `'0'+${result}`;
     }
     return {
       avatar: `img/avatars/user${result}.png`,
@@ -180,21 +180,23 @@ class CreateObjTwo {
     ARR.length = Math.floor(1 + Math.random() * (obj.length + 1 - 1));
 
     for (let i = 0; i <= ARR.length - 1; i++) {
-      function check() {
-        function random() {
-          return Math.floor(Math.random() * (obj.length - 1 + 1));
-        }
-
-        const NUM = random();
-
-        if (VALALLFEATURES.has(NUM)) {
-          return check();
-        } else {
-          VALALLFEATURES.add(NUM);
-          ARR[i] = obj.at(NUM);
-        }
-      }
       check();
+      function check() {
+        random()
+      }
+      function random() {
+        return Math.floor(Math.random() * (obj.length - 1 + 1));
+      }
+
+      const NUM = random();
+
+      if (VALALLFEATURES.has(NUM)) {
+        return check();
+      } else {
+        VALALLFEATURES.add(NUM);
+        ARR[i] = obj.at(NUM);
+      }
+
     }
     return ARR;
   }
