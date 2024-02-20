8f992135f6091525490b41bf43bc92deb2192777
IONIN66 <127221123+IONIN66@users.noreply.github.com>
2024-02-20 17:05:36 +0300

    ok

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@ const ALL_ID = new Set();
 const ALL_URL = new Set();
 const ALL_ID_COMMENTS = new Set();
 
-let id = CreateObj.creatNoRepNum(25, 1, CreateObj.allId);
+let id = createNoRepNum(1, 25);
 // let url = `photos/${CreateObj.creatNoRepNum(25, 1, CreateObj.allUrl)}.jpg`;
 // let description = CreateObj.descrip.at(
 //   CreateObj.creatNum(1, CreateObj.descrip.length - 1)
@@ -14,13 +14,13 @@ let id = CreateObj.creatNoRepNum(25, 1, CreateObj.allId);
 
 class CreateObj {
   constructor(id, url, options, likes, comments) {
-    this.id = CreateObj.creatNoRepNum(25, 1, CreateObj.allId);
-    this.url = `photos/${CreateObj.creatNoRepNum(25, 1, CreateObj.allUrl)}.jpg`;
-    this.description = CreateObj.descrip.at(
-      CreateObj.creatNum(1, CreateObj.descrip.length - 1)
-    );
-    this.likes = CreateObj.getLike(200, 15);
-    this.comments = CreateObj.getDataComments();
+    this.id = id;
+    // this.url = `photos/${CreateObj.creatNoRepNum(25, 1, CreateObj.allUrl)}.jpg`;
+    // this.description = CreateObj.descrip.at(
+    //   CreateObj.creatNum(1, CreateObj.descrip.length - 1)
+    // );
+    // this.likes = CreateObj.getLike(200, 15);
+    // this.comments = CreateObj.getDataComments();
   }
 }
 
